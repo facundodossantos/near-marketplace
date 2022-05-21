46bb36dab85bcec3f16e82053a05edd0484b2c44
Facundo <facundo.dossantos.18@gmail.com>
2022-05-21 13:19:00 -0300

    Updates

---
+++ precache-manifest.55302e76ec747f9536a6d0409014e645.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203852360e5818786ab02e6f80f555d4",
+    "url": "/near-marketplace/index.html"
+  },
+  {
+    "revision": "4f8dbdd568a245cc935b",
+    "url": "/near-marketplace/static/css/2.a3ce5f83.chunk.css"
+  },
+  {
+    "revision": "f0cbbf5d53447ac3f8a3",
+    "url": "/near-marketplace/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "4f8dbdd568a245cc935b",
+    "url": "/near-marketplace/static/js/2.4de92077.chunk.js"
+  },
+  {
+    "revision": "f0cbbf5d53447ac3f8a3",
+    "url": "/near-marketplace/static/js/main.3938f195.chunk.js"
+  },
+  {
+    "revision": "078830b16bf9feb6b0ab",
+    "url": "/near-marketplace/static/js/runtime~main.c3700b4c.js"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/near-marketplace/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/near-marketplace/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "bc90abf1d28c6c9af1d0bce7b7e13aef",
+    "url": "/near-marketplace/static/media/sandwich.bc90abf1.jpg"
+  }
+]);
